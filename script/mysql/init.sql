8cc8b6f7d1ef81f7411cc86fd21bdf3531760fa8
Yzinuo <434538202@qq.com>
2025-01-20 15:20:46 +0800

    [Feats]: finished the database design

--- script/mysql/init.sql
+++ script/mysql/init.sql
@@ -1,1 +1,227 @@
-create database Tiktok
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE DATABASE IF NOT EXISTS `TOKTIK_COMMERCE` DEFAULT  CHARACTER SET utf8mb4;
+USE `TOKTIK_COMMERCE`;
+-- ---------------------------
+-- ---------------------------
+-- 用户权限表
+DROP TABLE IF EXISTS `user_auth`;
+CREATE TABLE `user_auth`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) NULL DEFAULT NULL,
+  `updated_at` datetime(3) NULL DEFAULT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `ip_address` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '登录IP地址',
+  `ip_source` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'IP来源',
+  `is_disable` tinyint(1) NULL DEFAULT NULL,
+  `is_super` tinyint(1) NULL DEFAULT NULL,
+  `user_info_id` bigint NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `username`(`username`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) NULL DEFAULT NULL,
+  `updated_at` datetime(3) NULL DEFAULT NULL,
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `nickname` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `phone` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `avatar` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `intro` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `nickname`(`nickname` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `user_address`;
+CREATE TABLE  `user_address`(
+    `address_id`  bigint NOT NULL AUTO_INCREMENT,
+    `user_id`    bigint  NOT NULL DEFAULT 0,
+    `user_name`  varchar(30) NOT NULL DEFAULT '',
+    `phone`      varchar(30) NOT NULL DEFAULT '',
+    `default_flag` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否为默认 0-非默认 1-是默认',
+    `province_name` varchar(32) NOT NULL DEFAULT '' COMMENT '省',
+  `city_name` varchar(32) NOT NULL DEFAULT '' COMMENT '城',
+  `region_name` varchar(32) NOT NULL DEFAULT '' COMMENT '区',
+  `detail_address` varchar(64) NOT NULL DEFAULT '' COMMENT '收件详细地址(街道/楼宇/单元)',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除标识字段(0-未删除 1-已删除)',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  primary key(`address_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='收货地址表';
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) NULL DEFAULT NULL,
+  `updated_at` datetime(3) NULL DEFAULT NULL,
+  `name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `is_disable` tinyint(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `name`(`name` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `resource`;
+CREATE TABLE `resource`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) NULL DEFAULT NULL,
+  `updated_at` datetime(3) NULL DEFAULT NULL,
+  `parent_id` bigint NULL DEFAULT NULL,
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `method` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `anonymous` tinyint(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 117 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `role_resource`;
+CREATE TABLE `role_resource`  (
+  `resource_id` bigint NOT NULL,
+  `role_id` bigint NOT NULL,
+  PRIMARY KEY (`resource_id`, `role_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `user_auth_role`;
+CREATE TABLE `user_auth_role`  (
+  `user_auth_id` bigint NOT NULL,
+  `role_id` bigint NOT NULL,
+  PRIMARY KEY (`user_auth_id`, `role_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+
+-- ------------------------------------
+-- token表
+-- ------------------------------------
+
+DROP TABLE IF EXISTS `user_token`;
+CREATE TABLE `user_token` (
+  `admin_user_id` bigint(20) NOT NULL COMMENT '用户主键id',
+  `token` varchar(32) NOT NULL COMMENT 'token值(32位字符串)',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `expire_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'token过期时间',
+  PRIMARY KEY (`admin_user_id`),
+  UNIQUE KEY `uq_token` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ------------------------------------
+-- 商品表
+-- ------------------------------------
+DROP TABLE IF EXISTS `product_category`;
+CREATE TABLE `product_category` (
+  `category_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '分类id',
+  `category_name` varchar(50) NOT NULL DEFAULT '' COMMENT '分类名称',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除标识字段(0-未删除 1-已删除)',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`category_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+DROP TABLE IF EXISTS `product_info`;
+CREATE TABLE `product_info` (
+  `product_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '商品表主键id',
+  `product_name` varchar(200) NOT NULL DEFAULT '' COMMENT '商品名',
+  `product_intro` varchar(200) NOT NULL DEFAULT '' COMMENT '商品简介',
+  `product_category_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '关联分类id',
+  `product_cover_img` varchar(200) NOT NULL DEFAULT '/no-img.png' COMMENT '商品主图',
+  `product_carousel` varchar(500) NOT NULL DEFAULT '/no-img.png' COMMENT '商品轮播图',
+  `product_detail_content` text NOT NULL COMMENT '商品详情',
+  `original_price` int(11) NOT NULL DEFAULT '1' COMMENT '商品价格',
+  `selling_price` int(11) NOT NULL DEFAULT '1' COMMENT '商品实际售价',
+  `stock_num` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '商品库存数量',
+  `tag` varchar(20) NOT NULL DEFAULT '' COMMENT '商品标签',
+  `product_sell_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '商品上架状态 1-下架 0-上架',
+  `create_user` int(11) NOT NULL DEFAULT '0' COMMENT '添加者主键id',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '商品添加时间',
+  `update_user` int(11) NOT NULL DEFAULT '0' COMMENT '修改者主键id',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '商品修改时间',
+  PRIMARY KEY (`product_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+-- ------------------------------------
+-- 订单表
+-- ------------------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单表主键id',
+  `order_no` varchar(20) NOT NULL DEFAULT '' COMMENT '订单号',
+  `user_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '用户主键id',
+  `total_price` int(11) NOT NULL DEFAULT '1' COMMENT '订单总价',
+  `pay_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '支付状态:0.未支付,1.支付成功,-1:支付失败',
+  `pay_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0.无 1.支付宝支付 2.微信支付',
+  `pay_time` datetime DEFAULT NULL COMMENT '支付时间',
+  `order_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '订单状态:0.待支付 1.已支付 2.配货完成 3:出库成功 4.交易成功 -1.手动关闭 -2.超时关闭 -3.商家关闭',
+  `extra_info` varchar(100) NOT NULL DEFAULT '' COMMENT '订单body',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除标识字段(0-未删除 1-已删除)',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最新修改时间',
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `order_address`;
+CREATE TABLE `order_address` (
+  `order_id` bigint(20) NOT NULL,
+  `user_name` varchar(30) NOT NULL DEFAULT '' COMMENT '收货人姓名',
+  `user_phone` varchar(11) NOT NULL DEFAULT '' COMMENT '收货人手机号',
+  `province_name` varchar(32) NOT NULL DEFAULT '' COMMENT '省',
+  `city_name` varchar(32) NOT NULL DEFAULT '' COMMENT '城',
+  `region_name` varchar(32) NOT NULL DEFAULT '' COMMENT '区',
+  `detail_address` varchar(64) NOT NULL DEFAULT '' COMMENT '收件详细地址(街道/楼宇/单元)',
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单收货地址关联表';
+
+DROP TABLE IF EXISTS `order_item`;
+
+CREATE TABLE `order_item` (
+  `order_item_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单关联购物项主键id',
+  `order_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '订单主键id',
+  `goods_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '关联商品id',
+  `goods_name` varchar(200) NOT NULL DEFAULT '' COMMENT '下单时商品的名称(订单快照)',
+  `goods_cover_img` varchar(200) NOT NULL DEFAULT '' COMMENT '下单时商品的主图(订单快照)',
+  `selling_price` int(11) NOT NULL DEFAULT '1' COMMENT '下单时商品的价格(订单快照)',
+  `goods_count` int(11) NOT NULL DEFAULT '1' COMMENT '数量(订单快照)',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`order_item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ------------------------------------
+-- 支付 表
+-- ------------------------------------
+Drop TABLE IF EXISTS `payment_accounts`;
+CREATE TABLE payment_accounts (
+    account_id INT AUTO_INCREMENT,
+    user_id INT NOT NULL DEFAULT 0,
+    payment_type ENUM('alipay', 'wechat', 'bank') NOT NULL DEFAULT 'Wechat', -- 支付方式
+    balance DECIMAL(10, 2) DEFAULT 0.00, -- 余额
+    primary key (`account_id`)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- ------------------------------------
+-- 日志表
+-- ------------------------------------
+DROP TABLE IF EXISTS `operation_log`;
+CREATE TABLE `operation_log`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) NULL DEFAULT NULL,
+  `updated_at` datetime(3) NULL DEFAULT NULL,
+  `opt_type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '操作类型',
+  `opt_method` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '操作方法',
+  `opt_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '操作URL',
+  `opt_desc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '操作描述',
+  `request_param` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '请求参数',
+  `request_method` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '请求方法',
+  `response_data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '响应数据',
+  `user_id` bigint NULL DEFAULT NULL COMMENT '用户ID',
+  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户昵称',
+  `ip_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '操作IP',
+  `ip_source` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '操作ip地址',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
